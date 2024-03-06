3035c59a8879e61e9fef9ea311f8386ec03ba4b2
Valerie <sleeplessval@gmail.com>
2024-03-06 18:51:23 -0500

    fixed line breaks using the '\n' sequence not working

--- src/Program.cs
+++ src/Program.cs
@@ -2,22 +2,29 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 
+//	make sure fortune directory exists
 var resourcePath = "/usr/share/fortune-cs/";
 if(!Directory.Exists(resourcePath)) {
 	Console.WriteLine("fortune-cs: directory '/usr/share/fortune-cs/' does not exist");
 	return 1;
 }
 
+//	pull file list
 var files = Directory.GetFiles(resourcePath, "*.txt");
 
 var prng = RandomNumberGenerator.Create();
 
+//	choose a file and line
 var file = files[RandomNumberGenerator.GetInt32(files.Length)];
 var lines = File.ReadAllLines(file);
 var line = lines[RandomNumberGenerator.GetInt32(lines.Length)];
 
 prng.Dispose();
 
+//	process escape codes
+line = line.Replace("\\n", "\n");
+
+//	write
 Console.WriteLine(line);
 
 return 0;
